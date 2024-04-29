c4ddf7f903a2c5d5ad268c0f9f83cd6243921e16
Bhavay Aggarwal <bhavayaggarwal2412@gmail.com>
2024-04-29 18:25:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import pickle
 import streamlit as st
 
-'''def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
-'''
+#def fetch_poster(movie_id):
+#    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+#    data = requests.get(url)
+#    data = data.json()
+#    poster_path = data['poster_path']
+#    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+#    return full_path
+
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse = True, key=lambda x: x[1])
